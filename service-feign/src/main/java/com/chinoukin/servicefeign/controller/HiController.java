e66346cef790f89148211a1212fbdf7775141a41
chinoukin <chinoukin@qq.com>
2019-05-14 17:05:27 +0800

    增加spring的Environment的测试

--- service-feign/src/main/java/com/chinoukin/servicefeign/controller/HiController.java
+++ service-feign/src/main/java/com/chinoukin/servicefeign/controller/HiController.java
@@ -5,6 +5,7 @@ import com.chinoukin.servicefeign.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,24 @@ public class HiController {
 
     @Autowired
     SchedualServiceHi schedualServiceHi;
+
+    @Autowired
+    Environment environment;
+
+    @RequestMapping(value="/showEnv",method = RequestMethod.GET)
+    public String showEnv(){
+        System.out.println("----------activeProfiles------------");
+        String[] activeProfiles =  environment.getActiveProfiles();
+        for (String ap : activeProfiles) {
+            System.out.println(ap);
+        }
+        System.out.println("----------defaultProfiles------------");
+        String[] defaultProfiles = environment.getDefaultProfiles();
+        for (String dp : defaultProfiles) {
+            System.out.println(dp);
+        }
+        return "success";
+    }
 
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     public String sayHi(@RequestParam String name){
