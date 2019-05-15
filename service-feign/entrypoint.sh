d1e44c3ef6d973e5396ba06e1f08a7a03f0a8007
chinoukin <chinoukin@qq.com>
2019-05-15 11:10:23 +0800

    yaml中复杂的command改为编写并使用shell脚本的方式

--- service-feign/entrypoint.sh
+++ service-feign/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 bootJavaOpts=$(cat /etc/config/bootJavaOpts)
+echo 'bootJavaOPts:'$bootJavaOpts
 jvmOpts=$(cat /etc/config/jvmOpts)
+echo 'jvmOpts:'$jvmOpts
 
-java -Djava.security.egd=file:/dev/./urandom -jar /app.jar $(bootJavaOpts) $(jvmOpts)
+java -Djava.security.egd=file:/dev/./urandom -jar /app.jar $bootJavaOpts $jvmOpts
